f6460db52b85a26596d4804a31934be44d45d00e
WillBoudreau <williamboudreau804@gmail.com>
2024-09-06 09:25:16 -0400

    Finished test

--- Assets/CubeBehaviour.cs
+++ Assets/CubeBehaviour.cs
@@ -8,7 +8,7 @@ public class CubeBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CubeRotSpeed = 50.0f;
     }
 
     // Update is called once per frame
