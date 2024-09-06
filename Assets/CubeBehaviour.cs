9709352c1d2fbdd545554eae611ddf75e8b699da
WillBoudreau <williamboudreau804@gmail.com>
2024-09-06 09:19:55 -0400

    Created Test

---
+++ Assets/CubeBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    public float CubeRotSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CubeRotate();
+    }
+    void CubeRotate()
+    {
+        transform.Rotate(Vector3.up, CubeRotSpeed * Time.deltaTime);
+    }
+}
